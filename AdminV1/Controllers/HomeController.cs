3fedb9f7ccc8b8d47df13d954cedcb9f1a316a36
aXMediaSolutions <axmediasolutions@gmail.com>
2016-08-27 17:12:16 +1000

    Add project files.

---
+++ AdminV1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AdminV1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminV1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdminV1.MvcApplication" Language="C#" %>
